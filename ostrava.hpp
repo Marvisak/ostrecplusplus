086e7c15b6b1d7a1a0923bf09f96bd0d509f790a
Marvis <kukuczdaniel@gmail.com>
2022-06-24 22:23:21 +0200

    Sorac trochu mi to ujelo

---
+++ ostrava.hpp
@@ -0,0 +1,12 @@
+#define cyslo int
+
+#define standartna std
+#define standartne std
+#define standartny std
+
+#define vyplyvny cout
+#define tecka endl
+
+#define vracaj return
+
+#define hlavny main
