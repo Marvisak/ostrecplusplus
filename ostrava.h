807628511ed15785647415e528c3cd77d6d221d6
Marvis <kukuczdaniel@gmail.com>
2022-06-24 22:09:49 +0200

    Inicialozny vglad

---
+++ ostrava.h
@@ -0,0 +1,7 @@
+#define cyslo int
+
+#define vyplyvny printf
+
+#define vracaj return
+
+#define hlavny main
---
+++ pryklady/hlavny.c
@@ -0,0 +1,7 @@
+#include "../ostrava.h"
+#include <stdio.h>
+
+cyslo hlavny() {
+  vyplyvny("OSTRAVA!!!\n");
+  vracaj 0;
+}
